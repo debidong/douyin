873381327b16996987342b355cfa1e99e55e4637
debidong <ybw1217@outlook.com>
2023-08-25 14:58:44 +0800

    User model initialized.

---
+++ models/account.go
@@ -0,0 +1,12 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Username    string  `gorm:"unique;not null"`
+	Email       string  `gorm:"unique;not null"`
+	Password    string  `gorm:"not null"`
+	Subscribers []*User `gorm:"many2many:user_subscribers"`
+	Followers   []*User `gorm:"many2many:user_followers"`
+}
--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,19 @@
 package utils
+
+import (
+	"douyin/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func initDB() {
+	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to db.")
+	}
+
+	db = database
+	db.AutoMigrate(&models.User{})
+}
