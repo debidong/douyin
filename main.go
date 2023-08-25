f085f9663aab2d066196243bdb768744f417be06
debidong <ybw1217@outlook.com>
2023-08-25 18:23:34 +0800

    registration module improved.

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"douyin/utils"
 	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Service starting.")
 	utils.InitDB()
+	r := gin.Default()
+	initRouter(r)
+	err := r.Run()
+	if err != nil {
+		return
+	}
 }
---
+++ router.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/gin-gonic/gin"
+import "douyin/api/user"
+
+func initRouter(r *gin.Engine) {
+	apiRouter := r.Group("/douyin")
+	apiRouter.POST("/user/register/", user.Register)
+}
