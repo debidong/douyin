e5608511715c5af219bfb06e955f1af0526c5bc5
luoxu <1109633521@qq.com>
2023-08-31 21:12:26 +0800

    Basic design for friend

--- views/relation/friend.go
+++ views/relation/friend.go
@@ -1,1 +1,46 @@
 package relation
+
+import (
+	"douyin/models"
+	"douyin/utils"
+	"douyin/utils/auth"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type friendListResponse struct {
+	StatusCode int32         `json:"status_code"`
+	StatusMsg  string        `json:"status_msg"`
+	FriendList []models.User `json:"user_list"`
+}
+
+func FriendList(c *gin.Context) {
+	// get token
+	token := c.Query("token")
+	userId := c.Query("user_id")
+	username, err := auth.GetUserFromToken("token")
+	if err != nil {
+		resp := followListResponse{
+			StatusCode: 1,
+			StatusMsg:  "token解析失败,携带正确的token",
+			UserList:   nil,
+		}
+		c.JSON(http.StatusInternalServerError, resp)
+	}
+	fmt.Println(userId)
+	fmt.Println(username)
+	fmt.Println(token)
+	var userList []models.User
+	if err := utils.DB.Find(&userList).Error; err != nil {
+		response := friendListResponse{StatusCode: -2}
+		c.JSON(http.StatusUnauthorized, response)
+		return
+	}
+	resp := friendListResponse{
+		StatusCode: 0,
+		StatusMsg:  "查询成功",
+		FriendList: userList,
+	}
+	c.JSON(http.StatusOK, resp)
+}
