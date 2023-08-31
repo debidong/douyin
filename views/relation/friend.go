fe43781c5a5df685943f36ffc211ba3f7408a4fa
luoxu <1109633521@qq.com>
2023-08-31 23:01:43 +0800

    friend/list api almost finish

--- views/relation/friend.go
+++ views/relation/friend.go
@@ -2,39 +2,81 @@ package relation
 
 import (
 	"douyin/models"
+	"douyin/utils"
 	"douyin/utils/auth"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-type FriendListResponse struct {
-	StatusCode int32         `json:"status_code"`
-	StatusMsg  string        `json:"status_msg"`
-	FriendUser []models.User `json:"user_list"`
+type friendListResponse struct {
+	StatusCode int32        `json:"status_code"`
+	StatusMsg  string       `json:"status_msg"`
+	FriendList []UserFriend `json:"user_list"`
+}
+
+type UserFriend struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
 }
 
 func FriendList(c *gin.Context) {
-	// get token
+	// 解析获取用户
 	token := c.Query("token")
-	username, err := auth.GetUserFromToken(token)
+	_, err := auth.GetUserFromToken(token)
 	if err != nil {
-		resp := followListResponse{
+		resp := followerListResponse{
 			StatusCode: 1,
 			StatusMsg:  "token解析失败,携带正确的token",
 			UserList:   nil,
 		}
 		c.JSON(http.StatusInternalServerError, resp)
 	}
-	id := c.Query("user_id")
-	fmt.Println(id)
-	fmt.Println(username)
-	fmt.Println(token)
-	resp := followListResponse{
+	userId, err := utils.ParseParamToInt(c, "user_id")
+
+	//获取该用户关注的对象ID
+	var followerId []int64
+	if err := utils.DB.Table("user_followers").Select("follower_id").
+		Where("user_id = ?", userId).Find(&followerId).Error; err != nil {
+		response := friendListResponse{StatusCode: -1}
+		c.JSON(http.StatusNotFound, response)
+		return
+	}
+
+	//用关注对象id查follower获取好友（互关为好友
+	var friendId []int64
+	if err := utils.DB.Table("user_followers").Select("user_id").Where("user_id in (?)", followerId).
+		Where("follower_id = ?", userId).Find(&friendId).Error; err != nil {
+		response := friendListResponse{StatusCode: -1}
+		c.JSON(http.StatusNotFound, response)
+		return
+	}
+
+	//获取好友的用户信息
+	var friends []models.User
+	if err := utils.DB.Table("users").Where("user_id in (?)", friendId).
+		Find(&friends).Error; err != nil {
+		response := friendListResponse{StatusCode: -1}
+		c.JSON(http.StatusNotFound, response)
+		return
+	}
+
+	//组装查询结果
+	var users []UserFriend
+	for _, friend := range friends {
+		users = append(users, UserFriend{
+			ID:   friend.UserId,
+			Name: friend.Username,
+		})
+	}
+	fmt.Println(len(friends))
+	fmt.Println(len(users))
+
+	//封装查询成功结果
+	resp := friendListResponse{
 		StatusCode: 0,
 		StatusMsg:  "查询成功",
-		UserList:   nil,
+		FriendList: users,
 	}
 	c.JSON(http.StatusOK, resp)
-
 }
